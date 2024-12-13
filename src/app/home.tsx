6fc3af71b4120542826a179750728f21c7bb1877
Gabriel Porto <7gabrielporto@gmail.com>
2024-12-13 20:46:18 -0300

    feat: add markets and categories components on home page

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -4,11 +4,18 @@ import { useEffect, useState } from "react"
 import { api } from "@/services/api"
 
 import { Categories, CategoriesProps } from "@/components/categories"
+import { Places } from "@/components/places"
+import { PlaceProps } from "@/components/place"
+
+type MarketProps = PlaceProps & {
+
+}
 
 export  default function Home() {
 
     const [categories, setCategories] = useState<CategoriesProps>([])
     const [category, setCategory] = useState("")
+    const [markets, setMarkets] = useState<MarketProps[]>([])
 
     async function fetchCategories() {
         try {
@@ -17,7 +24,22 @@ export  default function Home() {
             setCategory(data[0].id)
         } catch (error) {
             console.log(error)
-            Alert.alert("Categorias","Não foi possível carregar as categorias.")
+            Alert.alert("Categorias", "Não foi possível carregar as categorias.")
+        }
+    }
+
+    async function fetchMarkets() {
+        try {
+            if(!category) {
+                return
+            }
+
+            const { data } = await api.get("/markets/category/" + category)
+            setMarkets(data)
+
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Locais", "Não foi possível carregar os locais.")
         }
     }
 
@@ -25,9 +47,21 @@ export  default function Home() {
         fetchCategories()
     }, [])
 
+    useEffect(() => {
+        fetchMarkets()
+    }, [category])
+
     return (
-        <View style={{ flex: 1}}>
-            <Categories data={categories} onSelect={setCategory} selected={category} />
+        <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
+            <Categories 
+                data={categories} 
+                onSelect={setCategory} 
+                selected={category} 
+            />
+
+            <Places data={markets} />
         </View>
+
+
     )
 }
