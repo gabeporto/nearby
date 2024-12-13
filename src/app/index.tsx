9cd1a0032cf2fab581c2f56441370865c7fe891f
Gabriel Porto <7gabrielporto@gmail.com>
2024-12-13 19:04:19 -0300

    feat: add onPress on index button page to redirect to home page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native"
-import { IconPlus } from "@tabler/icons-react-native"
+import { router } from "expo-router"
 
 import { Welcome } from "@/components/welcome"
 import { Steps } from "@/components/steps"
@@ -12,7 +12,7 @@ export default function Index() {
 
             <Steps />
 
-            <Button>
+            <Button onPress={() => router.navigate("/home")}>
                 <Button.Title>Começar</Button.Title>
             </Button>
         </View>
